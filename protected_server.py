34256d005970cd98c00bffec36327f0461edd8ab
MiguelMendesDA <miguelmendesdataanalyst@gmail.com>
2025-03-30 13:06:56 +0100

    Update

--- protected_server.py
+++ protected_server.py
@@ -249,5 +249,5 @@ def update():
         return jsonify({'error': f'Observation ID "{obs["observation_id"]}" does not exist'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5010)
 
